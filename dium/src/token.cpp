628d0c6519c0d5934e255f9a152b112c0f172865
Kyle Chapman <kyleichapman@gmail.com>
2022-07-23 21:33:43 +0200

    Implement token to string conversion

---
+++ dium/src/token.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file       token.hpp
+ * @brief      Data type definitions for the lexer
+ * @copyright  Copyright (c) 2022-present
+ * @author     Kyle Chapman
+ * @date       2022-07-23
+ */
+
+#include <cassert>
+#include "token.hpp"
+
+/* Names of each recognized token */
+static const char *tokenNames[] = {
+	"end-of-file", "identifier", "boolean", "character", "string", "number", "decimal", "array",
+	"'and'", "'break'", "'continue'", "'else'", "'elsif'", "'exit'", "'false'", "'for'", "'func'",
+	"'if'", "'or'", "'print'", "'println'", "'return'", "'true'", "'void'", "'while'",
+	"'='", "'>='", "'>'", "'<='", "'<'", "'!='", "'-'", "'+'", "'/'", "'*'", "'%'",
+	"'['", "']'", "','", "'('", "')'", "'=>'"
+};
+
+const char *getTokenString(TokenType type) {
+	assert(type >= -1 && type < (sizeof(tokenNames) / sizeof(char *)));
+	return tokenNames[type];
+}
